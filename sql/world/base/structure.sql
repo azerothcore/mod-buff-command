f2b4183b889edc28254bac60e09ba9ed09d4fcaf
BarbzYHOOL <professor_barbz@gmx.com>
2020-01-11 17:52:28 +0100

    chore: Moved SQL to a correct structure

--- sql/world/base/structure.sql
+++ sql/world/base/structure.sql
@@ -1,6 +1,4 @@
--- ----------------------------
--- Table structure for player_buff
--- ----------------------------
+-- STRUCTURE
 DROP TABLE IF EXISTS `player_buff`;
 CREATE TABLE `player_buff`  (
   `spellid` int(10) NOT NULL COMMENT 'Spell ID',
@@ -8,9 +6,7 @@ CREATE TABLE `player_buff`  (
   PRIMARY KEY (`spellid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
--- ----------------------------
--- Records of player_buff
--- ----------------------------
+-- DATA
 INSERT INTO `player_buff` VALUES (25898, NULL);
 INSERT INTO `player_buff` VALUES (48074, NULL);
 INSERT INTO `player_buff` VALUES (48162, NULL);
@@ -18,4 +14,5 @@ INSERT INTO `player_buff` VALUES (48470, NULL);
 INSERT INTO `player_buff` VALUES (57623, NULL);
 INSERT INTO `player_buff` VALUES (61316, NULL);
 
+-- COMMAND
 INSERT INTO `command`(`name`, `security`, `help`) VALUES ('buff', 0, 'Syntax: .buff. Buff player.\r\n.buff reload - reload player_buff table.');
